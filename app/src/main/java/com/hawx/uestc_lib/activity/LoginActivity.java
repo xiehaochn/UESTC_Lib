0373375da5e131df086c42ebde0fb70d00087a2f
HawX <xiehaochina@vip.qq.com>
2016-05-17 21:41:59 +0800

    整合登录功能，自动登录以及记住账户功能有待添加。

--- app/src/main/java/com/hawx/uestc_lib/activity/LoginActivity.java
+++ app/src/main/java/com/hawx/uestc_lib/activity/LoginActivity.java
@@ -1,25 +1,104 @@
 package com.hawx.uestc_lib.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.hawx.uestc_lib.R;
 import com.hawx.uestc_lib.base.BaseActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.CookieManager;
+import java.net.HttpCookie;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by Administrator on 2016/5/12.
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    private final String USER_DATA="USER_DATA";
+    private final String FROM_ACTIVITY="FROM_ACTIVITY";
     private Toolbar toolbar;
+    @BindView(R.id.activity_login_uername)
+    EditText userName;
+    @BindView(R.id.activity_login_password)
+    EditText passWord;
+    @BindView(R.id.activity_login_checkbox1)
+    CheckBox remember;
+    @BindView(R.id.activity_login_checkbox2)
+    CheckBox autoLogin;
+    @BindView(R.id.activity_login_button_log)
+    Button login;
+    @BindView(R.id.activity_login_button_back)
+    Button back;
+    private Handler mhandler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:{
+                    String user_data=msg.getData().getString("user_data");
+                    startUserCenterActivity(LoginActivity.this,user_data);
+                    break;
+                }
+                case 2:{
+                    toast("连接超时，请检查网络设置");
+                    break;
+                }
+                case 3:{
+                    toast("帐号信息有误");
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    };
+
+    private void startUserCenterActivity(Context context, String user_data) {
+        Bundle bundle=new Bundle();
+        bundle.putString(USER_DATA,user_data);
+        bundle.putString(FROM_ACTIVITY,"LoginActivity");
+        Intent intent=new Intent(context,UserCenterActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setNeedLeftMenu(false);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
         initToolBar();
+        initView();
     }
+
+    private void initView() {
+        back.setOnClickListener(this);
+        login.setOnClickListener(this);
+
+    }
+
     private void initToolBar() {
         toolbar = (Toolbar) findViewById(R.id.base_toolbar);
         toolbar.setTitle(R.string.app_name);
@@ -33,5 +112,95 @@ public class LoginActivity extends BaseActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activity_login_button_back:{
+                finish();
+                break;
+            }
+            case R.id.activity_login_button_log:{
+                String user_id=userName.getText().toString();
+                String user_pw=passWord.getText().toString();
+                if(user_id.equals("")|user_pw.equals("")){
+                    toast("请输入帐号信息");
+                }else {
+                    saveUserData();
+                    final String url = "https://webpac.uestc.edu.cn/patroninfo*chx?extpatid=" + user_id + "&extpatpw=" + user_pw + "&submit=submit";
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            HttpsURLConnection con1 = null;
+                            HttpsURLConnection con = null;
+                            try {
+                                URL url_URL = new URL(url);
+                                con1 = (HttpsURLConnection) url_URL.openConnection();
+                                con1.setInstanceFollowRedirects(false);
+                                con1.setRequestMethod("GET");
+                                con1.setConnectTimeout(8000);
+                                con1.setReadTimeout(8000);
+                                String COOKIES_HEADER = "Set-Cookie";
+                                CookieManager msCookieManager = new CookieManager();
+                                Map<String, List<String>> headerFields = con1.getHeaderFields();
+                                List<String> cookiesHeader = headerFields.get(COOKIES_HEADER);
+                                if (cookiesHeader != null) {
+                                    for (String cookie : cookiesHeader) {
+                                        msCookieManager.getCookieStore().add(null, HttpCookie.parse(cookie).get(0));
+                                    }
+                                }
+                                String header_location = con1.getHeaderField("Location");
+                                if (header_location != null) {
+                                    String url_detail = "https://webpac.uestc.edu.cn" + header_location;
+                                    //===================================================================================
+                                    URL url_URL_detail = new URL(url_detail);
+                                    con = (HttpsURLConnection) url_URL_detail.openConnection();
+                                    con.setRequestMethod("GET");
+                                    if (msCookieManager.getCookieStore().getCookies().size() > 0) {
+                                        con.setRequestProperty("Cookie",
+                                                TextUtils.join(";", msCookieManager.getCookieStore().getCookies()));
+                                    }
+                                    con.setConnectTimeout(8000);
+                                    con.setReadTimeout(8000);
+                                    InputStream in = con.getInputStream();
+                                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                                    StringBuilder builder = new StringBuilder();
+                                    String line;
+                                    while ((line = reader.readLine()) != null) {
+                                        builder.append(line);
+                                    }
+                                    String response = builder.toString();
+                                    Message msg = new Message();
+                                    msg.what = 1;
+                                    Bundle bundle = new Bundle();
+                                    bundle.putString("user_data", response);
+                                    msg.setData(bundle);
+                                    mhandler.sendMessage(msg);
+                                } else {
+                                    mhandler.sendEmptyMessage(3);
+                                }
+
+                            } catch (SocketTimeoutException e) {
+                                // TODO Auto-generated catch block
+                                mhandler.sendEmptyMessage(2);
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private void saveUserData() {
+        if(remember.isChecked()){
+            //to be continued...
+        }
     }
 }
--- app/src/main/java/com/hawx/uestc_lib/activity/UserCenterActivity.java
+++ app/src/main/java/com/hawx/uestc_lib/activity/UserCenterActivity.java
@@ -1,19 +1,139 @@
 package com.hawx.uestc_lib.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.hawx.uestc_lib.R;
 import com.hawx.uestc_lib.base.BaseActivity;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2016/5/12.
  */
 public class UserCenterActivity extends BaseActivity {
+    private final String USER_DATA="USER_DATA";
+    private final String FROM_ACTIVITY="FROM_ACTIVITY";
+    private String user_name;
+    private int bookNum;
+    private ArrayList<String> BookDetail=new ArrayList<String>();
+    @BindView(R.id.activity_usercenter_name)
+    TextView name;
+    @BindView(R.id.activity_usercenter_school)
+    TextView school;
+    @BindView(R.id.activity_usercenter_date)
+    TextView date;
+    @BindView(R.id.activity_usercenter_date_container)
+    LinearLayout dataContainer;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTag("个人中心");
         setContentView(R.layout.activity_usercenter);
+        ButterKnife.bind(this);
+        if(getIntent().getExtras()!=null) {
+            String from=getIntent().getExtras().getString(FROM_ACTIVITY);
+            if (from.equals("LoginActivity")) {
+                parseData();
+            } else {
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }else{
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void parseData() {
+        Bundle bundle=this.getIntent().getExtras();
+        String data=bundle.getString(USER_DATA);
+        Document doc = Jsoup.parse(data);
+        Elements user =doc.getElementsByClass("patNameAddress");
+        for (Element user_one :user ) {
+            Elements name=user_one.getElementsByTag("strong");
+            for (Element name_one :name ) {
+                user_name=name_one.text();
+            }
+        }
+        name.setText(user_name);
+        String user_string=user.text();
+        String school_regular = "("+user_name+")"+"(.*)(证件)";
+        Pattern school_pattern = Pattern.compile(school_regular);
+        Matcher school_m = school_pattern.matcher(user_string);
+        if (school_m.find( )) {
+            school.setText(school_m.group(2));
+        } else {
+            school.setText("没有相关信息");
+        }
+        String date_regular = "(证件)(.*)(续借)";
+        Pattern date_pattern = Pattern.compile(date_regular);
+        Matcher date_m = date_pattern.matcher(user_string);
+        if (date_m.find( )) {
+            date.setText(date_m.group(2));
+        } else {
+            date.setText("没有相关信息");
+        }
+        Elements book =doc.getElementsByClass("patFuncEntry");
+        bookNum=0;
+        for (Element book_one :book ) {
+            bookNum+=1;
+            Elements book_detail=book_one.getElementsByTag("td");
+            for (Element book_final:book_detail ) {
+                String test=book_final.text();
+                if(test.equals("")){
+
+                } else {
+                    BookDetail.add(test);
+                }
+            }
+        }
+        setTable();
+    }
+
+    private void setTable() {
+        TableLayoutDate tl;
+        for(int i=0;i<bookNum;i++){
+            tl=generateTableView(i);
+            dataContainer.addView(tl);
+        }
+    }
+    private TableLayoutDate generateTableView(int i) {
+        TableLayoutDate tl_r=new TableLayoutDate(this,BookDetail.get(i*4),BookDetail.get(i*4+1),BookDetail.get(i*4+2));
+        return tl_r;
+    }
+    class TableLayoutDate extends LinearLayout{
+        public TableLayoutDate(Context context){
+            super(context);
+
+        }
+        public TableLayoutDate(Context context,String name,String num,String date){
+            super(context);
+            LayoutInflater.from(context).inflate(R.layout.widget_tablelayout_borrowedbook,this);
+            TextView tv1= (TextView) findViewById(R.id.widget_tablelayout_borrowed_name);
+            TextView tv2= (TextView) findViewById(R.id.widget_tablelayout_borrowed_num);
+            TextView tv3= (TextView) findViewById(R.id.widget_tablelayout_borrowed_state);
+            tv1.setText(name);
+            tv2.setText(num);
+            tv3.setText(date);
+        }
+
     }
 }
